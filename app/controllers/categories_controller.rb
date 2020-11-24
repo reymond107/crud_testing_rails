6e7d35704406cdaf6a062504627f72a24fe5ef6c
reymond107 <rey@DESKTOP-DSHCCU3.localdomain>
2020-11-24 20:40:26 +0800

    add 2 space indentation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,53 +1,53 @@
 class CategoriesController < ApplicationController
 
-    def index
-        @categories = Category.all
-    end
+	def index
+		@categories = Category.all
+	end
 
-    def show
-        @category = Category.find_by(params[:id])
-    end
+	def show
+		@category = Category.find_by(params[:id])
+	end
 
-    def new
-        @category = Category.new
-    end
+	def new
+		@category = Category.new
+	end
 
-    def create
-        @category = Category.new(category_params)
+	def create
+		@category = Category.new(category_params)
 
-        if @category.save
-            redirect_to categories_path
-        else
-            render :new
-        end
-    end
+		if @category.save
+			redirect_to categories_path
+		else
+			render :new
+		end
+	end
 
-    def edit
-        @category = Category.find_by(params[:id])
-    end
+	def edit
+		@category = Category.find_by(params[:id])
+	end
 
-    def update
-        @category = Category.find_by(params[:id])
-    
-          if @category.update(category_params)
-              redirect_to categories_path
-          else
-              render :edit
-          end
-    end
+	def update
+		@category = Category.find_by(params[:id])
+	
+		  if @category.update(category_params)
+			  redirect_to categories_path
+		  else
+			  render :edit
+		  end
+	end
 
-    def delete
-        @category = Category.find(params[:id])  
-        if @category.destroy
-            redirect_to categories_path
-        else
-            render :edit
-        end 
-    end
+	def delete
+		@category = Category.find(params[:id])  
+		if @category.destroy
+			redirect_to categories_path
+		else
+			render :edit
+		end 
+	end
 
-    private
+	private
 
-    def category_params
-        params.require(:category).permit(:name, :description) 
-    end
+	def category_params
+		params.require(:category).permit(:name, :description) 
+	end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,26 +1,22 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "shoul not save without name" do
-  #   category = Category.new
-  #   assert_not category.save
-  # end
 
-  test "Category should save" do
-    category = Category.new(name: 'sample name', description: 'sample description')
-    assert category.save
-  end
+	test "Category should save" do
+		category = Category.new(name: 'sample name', description: 'sample description')
+		assert category.save
+	end
 
-  test "Category should not save without a name" do
-    category = Category.new
-    assert_not category.save
-  end
+	test "Category should not save without a name" do
+		category = Category.new
+		assert_not category.save
+	end
 
-  test "Category name should be unique" do
-    original = Category.new(name: 'unique name')
-    duplicate = Category.new(description: 'sample description')
+	test "Category name should be unique" do
+		original = Category.new(name: 'unique name')
+		duplicate = Category.new(description: 'sample description')
 
-    original.save
-    assert_not duplicate.save
-  end
+		original.save
+		assert_not duplicate.save
+	end
 end
